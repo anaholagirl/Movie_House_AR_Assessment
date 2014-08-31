66d68cc16557fd49774a7ff7361a5b986257d431
Linda Gibson <dakidz@live.com>
2014-08-30 18:36:59 -0700

    validate presence of rating name

--- lib/rating.rb
+++ lib/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ActiveRecord::Base
   has_many :movies
+
+  validates :name, :presence => true
 end
--- spec/rating_spec.rb
+++ spec/rating_spec.rb
@@ -7,4 +7,9 @@ describe Rating do
     test_movie2 = Movie.create({:name => 'The Avengers', :rating_id => test_rating.id})
     expect(test_rating.movies).to eq [test_movie1, test_movie2]
   end
+
+  it 'validates the presence of name' do
+    test_rating = Rating.new({:name => 'PG13'})
+    expect(test_rating.save).to eq true
+  end
 end
