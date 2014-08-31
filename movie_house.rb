5812e5409b848709d37ca3ab4e6912b65d1d8e35
Linda Gibson <dakidz@live.com>
2014-08-30 18:46:07 -0700

    add main menu to user interface

--- movie_house.rb
+++ movie_house.rb
@@ -23,4 +23,35 @@ def welcome
     puts "Please enter a valid option"
   end
 end
+
+def main_menu
+  puts "\n\n******** MAIN MENU ********\n\n"
+  puts "Press the corresponding letter to select choice\n\n"
+  puts "[a] Add a movie for the current movie listing"
+  puts "[l] List all movies in the current movie listing"
+  puts "[d] Remove a movie from the current movie listing"
+  puts "[c] Add a new customer for the current day"
+  puts "[v] View all customers and what movie they saw for the current day"
+  puts "[s] Search for a movie and see how many customers came to see it"
+  puts "[w] Return to the welcome menu"
+  menu_choice = gets.chomp.downcase
+  if menu_choice == 'a'
+    add_movie
+  elsif menu_choice == 'l'
+    list_movies
+  elsif menu_choice == 'd'
+    delete_movie
+  elsif menu_choice == 'c'
+    add_customer
+  elsif menu_choice == 'v'
+    list_customers
+  elsif menu_choice == 's'
+    search_movie
+  elsif menu_choice == 'w'
+    welcome
+  else
+    puts "Please enter a valid option"
+    main_menu
+  end
+end
 welcome
