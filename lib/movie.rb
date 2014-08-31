c72a37d9e08bf8d9162d55cd4ef2d5f99f984ff7
Linda Gibson <dakidz@live.com>
2014-08-30 18:14:16 -0700

    establish association that a movie belongs to a rating

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ActiveRecord::Base
   has_many :customers
+  belongs_to :rating
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -7,4 +7,10 @@ describe Movie do
     test_customer2 = Customer.create({:name => 'Ben Kenobi', :movie_id => test_movie.id})
     expect(test_movie.customers).to eq [test_customer1, test_customer2]
   end
+
+  it 'belongs to a rating' do
+    test_rating = Rating.create({:name => 'PG13'})
+    test_movie = Movie.create({:name => 'Star Wars', :rating_id => test_rating.id})
+    expect(test_movie.rating).to eq test_rating
+  end
 end
